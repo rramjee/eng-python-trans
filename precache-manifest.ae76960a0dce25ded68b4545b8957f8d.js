9138296dc6afa7d0600e46fbb2cf775171fc5352
rramjee@gmail.com <rramjee@gmail.com>
2021-03-19 21:31:01 +0530

    Updates

---
+++ precache-manifest.ae76960a0dce25ded68b4545b8957f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "642822d77162c39753eec301cb9df96d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "616fe29224524ff4c1da",
+    "url": "./static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3d55cd4dd728860b6668",
+    "url": "./static/js/2.cf9a94ac.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "./static/js/2.cf9a94ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616fe29224524ff4c1da",
+    "url": "./static/js/main.293f0cc9.chunk.js"
+  },
+  {
+    "revision": "39d890a9758519152bbe",
+    "url": "./static/js/runtime-main.0f4412ab.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  }
+]);
