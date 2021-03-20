e6cc3c11b4f6a92081b6a5665664476837dfc1e1
rramjee@gmail.com <rramjee@gmail.com>
2021-03-20 19:36:02 +0530

    Deploy to ordinary repo

--- src/App.js
+++ src/App.js
@@ -26,9 +26,12 @@ function App() {
   }
 
   return (
-    <div style={{height:"100vh", width:"100vw", alignItems:"center", justifyContent:"center", textAlign:"center", backgroundImage: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)", display:"table-cell", verticalAlign: "middle", horizontalAlign:"middle"}}>
-        <Input transparent onChange={handleValueChange} action={{color: 'teal', onClick: () => handleClick(), content: "Get Python Code Now"}} placeholder='Write...' style={{border:"1px solid #fff", padding:"10px", borderRadius: "5px"}} />
-        <p className="sentiment-text" style={{marginTop: "20px", color: "white"}}>No Sentiment</p>
+    <div style={{height:"100vh", width:"200vw", alignItems:"center", justifyContent:"center", textAlign:"center", backgroundImage: "linear-gradient(135deg, #667eea 0%, #764ba2 100%)", display:"table-cell", verticalAlign: "middle", horizontalAlign:"middle"}}>
+        <h1>Text to Python Generator</h1>
+        <Input transparent onChange={handleValueChange} action={{color: 'teal', onClick: () => handleClick(), content: "Get Python Code Now"}} placeholder='Write...' style={{border:"1px solid #fff", padding:"10px", borderRadius: "5px", width: "1000px"}} />
+        <p  style={{marginTop: "20px", color: "white"}}></p>
+        <Input className="sentiment-text" transparent onChange={handleValueChange}  style={{border:"1px solid #fff",background:"black",color:"white",  textAlign:"left", padding:"10px", borderRadius: "5px", width: "1000px", height:"300px"}} />
+        
     </div>
   );
 }
